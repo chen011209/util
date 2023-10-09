7787397f2f50594178418e1b4beebc03c26deec5
chen011209 <cyc7397221>
2023-10-09 10:20:04 +0800

    移动app文件位置

---
+++ src/main/java/com/example/demo/util/apps/git/main.java
@@ -0,0 +1,13 @@
+package com.example.demo.util.apps.git;
+
+/**
+ * 使用场景：备份 版本回退 协同开发 代码追踪
+ *
+ * idea连接github时先从github登录 后push 不能登录vpn
+ *
+ * git是分布式的 svn是集中式的 区别在git所有版本在每个电脑里都存在 而svn需要连服务器才能切换版本
+ *
+ * git结构分为远程仓库 本地仓库 暂存区域 工作目录
+ */
+public class main {
+}
