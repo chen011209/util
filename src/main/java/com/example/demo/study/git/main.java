c8ebf615bcb16cae6aa9eda7ba5c0ef74bf0ee7e
chen011209 <cyc7397221>
2023-10-10 11:26:57 +0800

    git

--- src/main/java/com/example/demo/study/git/main.java
+++
@@ -1,7 +0,0 @@
-package com.example.demo.study.git;
-
-/**
- * 学习办公等 office 截图 录屏等 修订版
- */
-public class main {
-}
--- src/main/java/com/example/demo/util/apps/git/main.java
+++ src/main/java/com/example/demo/util/apps/git/main.java
@@ -6,6 +6,9 @@ package com.example.demo.util.apps.git;
  * git是分布式的 svn是集中式的 区别在git所有版本在每个电脑里都存在 而svn需要连服务器才能切换版本
  *
  * git结构分为远程仓库 本地仓库 暂存区域 工作目录
+ *
+ * git回退直接check out revision 到最新的也check out到最新的版本 回退之前先提交代码
+ *
  */
 public class main {
 }
