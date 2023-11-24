8d6eb55a7739c18c1053bc68bb46a64cf679b5fc
chen011209 <cyc7397221>
2023-11-24 17:31:49 +0800

    todo

--- src/main/java/com/example/demo/todo.java
+++ src/main/java/com/example/demo/todo.java
@@ -6,7 +6,11 @@ import java.util.HashMap;
 
 public class todo {
 
-    // TODO: 2023/11/10 idea的learning Project 
+    // TODO: 2023/11/10 idea的learning Project
+    // TODO: 2023/11/23 分布式的锁
+    // TODO: 2023/11/23 rabbitMq队列何时创建 生产者 消费者？ 回调创建队列？
+    // TODO: 2023/11/23 idea查看类继承 实现
+    // TODO: 2023/11/23 idea查看重写类的方法被调用的时间
 
 
     public static void main(String[] args) throws Exception {
